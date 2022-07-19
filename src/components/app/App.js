81fff6e23e9070f43deeda3f336a252c6235d891
maksad <maxkavlanov110@gmail.com>
2022-07-19 16:12:55 +0500

    first commit

---
+++ src/components/app/App.js
@@ -0,0 +1,16 @@
+import '../app/App.scss';
+import Header from '../header/header';
+function App() {
+  return (
+    <div className='app'>
+      <div className='Header'>
+        <Header/>
+      </div>
+      <div className='store'>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header/Header.js
@@ -0,0 +1,8 @@
+// импортируешь что хочешь
+
+const Header = () => {
+    return(
+        <h2>Header</h2>
+    )
+}
+export default Header
